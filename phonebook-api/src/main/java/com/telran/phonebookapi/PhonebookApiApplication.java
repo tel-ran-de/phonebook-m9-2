eba1aa31c1ee15c42567cbdbeac2da7b8b9e312e
Evgeny Sharfarets <evgeny.sharfarets@gmail.com>
2020-06-03 12:44:53 +0200

    init

---
+++ phonebook-api/src/main/java/com/telran/phonebookapi/PhonebookApiApplication.java
@@ -0,0 +1,13 @@
+package com.telran.phonebookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhonebookApiApplication.class, args);
+    }
+
+}
