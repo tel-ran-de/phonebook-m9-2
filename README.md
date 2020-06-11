c0b413889be987a7fd1de1515002db3fbcb9d7d2
Gulnara <40672445+bolokadze@users.noreply.github.com>
2020-06-11 10:35:58 +0200

    Pb 2 (#1)
    * Create README.md
    
    * Adds informational model to the project

---
+++ README.md
@@ -0,0 +1,4 @@
+# phonebook-m9-2
+The project of morning 9-2 group
+
+Here should lie all the steps to run the project on a local machine.
---
+++ phonebook-api/src/main/java/com/telran/phonebookapi/model/Contact.java
@@ -0,0 +1,60 @@
+package com.telran.phonebookapi.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    @Setter
+    private String firstName;
+    @Setter
+    private String lastName;
+    @Setter
+    private String description;
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    @ElementCollection
+    private List<String> emails = new ArrayList<>();
+
+    @OneToMany(mappedBy = "contact", cascade = CascadeType.REMOVE)
+    private List<Phone> phones = new ArrayList<>();
+
+    @OneToMany(mappedBy = "contact", cascade = CascadeType.REMOVE)
+    private List<Address> addresses = new ArrayList<>();
+
+    public Contact(String firstName, User user) {
+        this.firstName = firstName;
+        this.user = user;
+
+    }
+
+    public void addAddress(Address address) {
+        addresses.add(address);
+    }
+
+    public void addPhone(Phone phone) {
+        phones.add(phone);
+    }
+
+    public void addEmail(String email) {
+        emails.add(email);
+    }
+
+}
+
