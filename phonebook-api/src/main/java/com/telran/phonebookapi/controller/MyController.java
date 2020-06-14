ae08e8aab5017289c77e0c020f5df9ea8c5ee437
innysikd <innysik@hotmail.com>
2020-06-15 01:43:56 +0200

    add mailing service

--- phonebook-api/src/main/java/com/telran/phonebookapi/controller/MyController.java
+++ phonebook-api/src/main/java/com/telran/phonebookapi/controller/MyController.java
@@ -1,4 +1,21 @@
 package com.telran.phonebookapi.controller;
 
+import com.telran.phonebookapi.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MyController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @GetMapping(value = "/sendmail")
+    public String sendmail() {
+
+        emailService.sendMail("innysik@gmail.com", "Test Subject", "Test mail");
+
+        return "emailsent";
+    }
 }
--- phonebook-api/src/test/java/com/telran/phonebookapi/PhonebookApiApplicationTests.java
+++ phonebook-api/src/test/java/com/telran/phonebookapi/PhonebookApiApplicationTests.java
@@ -1,13 +1,20 @@
 package com.telran.phonebookapi;
 
+import com.telran.phonebookapi.service.EmailService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 class PhonebookApiApplicationTests {
 
+    @Autowired
+    private EmailService emailService;
+
     @Test
-    void contextLoads() {
+    public void testEmail() {
+        emailService.sendMail("innysik@gmail.com", "Test subject", "Test mail");
     }
 
 }
