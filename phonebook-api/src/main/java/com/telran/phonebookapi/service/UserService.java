5a351040ad1797b1322ecaf39479c0f1b83505a3
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-08-03 10:05:51 +0200

    adds WebSecurityConfig class with BCryptPasswordEncoder and user password encoded adds errorHandler with new Exceptions, edit throwable exceptions in UserService

--- phonebook-api/src/main/java/com/telran/phonebookapi/service/UserService.java
+++ phonebook-api/src/main/java/com/telran/phonebookapi/service/UserService.java
@@ -1,6 +1,9 @@
 package com.telran.phonebookapi.service;
 
 import com.telran.phonebookapi.dto.UserDto;
+import com.telran.phonebookapi.exception.TokenNotFoundException;
+import com.telran.phonebookapi.exception.UserAlreadyExistsException;
+import com.telran.phonebookapi.exception.UserNotFoundException;
 import com.telran.phonebookapi.model.ActivationToken;
 import com.telran.phonebookapi.model.RecoveryToken;
 import com.telran.phonebookapi.model.User;
@@ -8,6 +11,7 @@ import com.telran.phonebookapi.persistance.IActivationTokenRepository;
 import com.telran.phonebookapi.persistance.IRecoveryTokenRepository;
 import com.telran.phonebookapi.persistance.IUserRepository;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityExistsException;
@@ -22,36 +26,39 @@ public class UserService {
     static final String ACTIVATION_MESSAGE = "Please, follow the link to activate your account: ";
     static final String NOT_ACTIVE_LINK = "Your link is not active anymore";
     static final String USER_DOES_NOT_EXIST = "Error! This user doesn't exist in our DB";
-    static final String INCORRECT_TOKEN = "Error! Registration failed: Invalid confirmation token";
     static final String RECOVER_YOUR_PASSWORD_MESSAGE = "Please click the link to recover your password ";
     static final String UI_RECOVERY_LINK = "/user/password-recovery/";
     static final String UI_ACTIVATION_LINK = "/user/activation/";
 
     @Value("${com.telran.phonebook.ui.host}")
-    String uiHost;
+    private String uiHost;
 
-    final IUserRepository userRepository;
-    final IActivationTokenRepository activationTokenRepository;
-    final IRecoveryTokenRepository recoveryTokenRepository;
-    final EmailSender emailSender;
+    private final IUserRepository userRepository;
+    private final IActivationTokenRepository activationTokenRepository;
+    private final IRecoveryTokenRepository recoveryTokenRepository;
+    private final EmailSender emailSender;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 
     public UserService(IUserRepository userRepository,
                        IActivationTokenRepository activationTokenRepository,
                        EmailSender emailSender,
-                       IRecoveryTokenRepository recoveryTokenRepository) {
+                       IRecoveryTokenRepository recoveryTokenRepository,
+                       BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.activationTokenRepository = activationTokenRepository;
         this.emailSender = emailSender;
         this.recoveryTokenRepository = recoveryTokenRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     public void addUser(UserDto userDto) {
         if (userRepository.findById(userDto.email).isPresent()) {
-            throw new EntityExistsException(USER_ALREADY_EXISTS);
+            throw new UserAlreadyExistsException(USER_ALREADY_EXISTS);
         } else {
             String token = UUID.randomUUID().toString();
-            User user = new User(userDto.email, userDto.password);
+            String encodedPassword = bCryptPasswordEncoder.encode(userDto.password);
+            User user = new User(userDto.email, encodedPassword);
             user.setActive(false);
             userRepository.save(user);
             activationTokenRepository.save(new ActivationToken(token, user));
@@ -62,7 +69,7 @@ public class UserService {
 
     public void activateUser(String token) {
         ActivationToken activationToken = activationTokenRepository.findById(token).
-                orElseThrow(() -> new EntityNotFoundException(NOT_ACTIVE_LINK));
+                orElseThrow(() -> new TokenNotFoundException(NOT_ACTIVE_LINK));
         User user = activationToken.getUser();
         user.setActive(true);
         userRepository.save(user);
@@ -70,7 +77,7 @@ public class UserService {
     }
 
     public void sendRecoveryToken(String email) {
-        User ourUser = userRepository.findById(email).orElseThrow(() -> new EntityNotFoundException(USER_DOES_NOT_EXIST));
+        User ourUser = userRepository.findById(email).orElseThrow(() -> new UserNotFoundException(USER_DOES_NOT_EXIST));
         String token = UUID.randomUUID().toString();
         RecoveryToken recoveryToken = new RecoveryToken(token, ourUser);
         recoveryTokenRepository.save(recoveryToken);
@@ -81,9 +88,11 @@ public class UserService {
     }
 
     public void createNewPassword(String recoveryToken, String password) {
-        RecoveryToken token = recoveryTokenRepository.findById(recoveryToken).orElseThrow(() -> new EntityNotFoundException(INCORRECT_TOKEN));
+        RecoveryToken token = recoveryTokenRepository.findById(recoveryToken).orElseThrow(() -> new TokenNotFoundException(NOT_ACTIVE_LINK));
         User ourUser = token.getUser();
-        ourUser.setPassword(password);
+        final String encryptedPassword = bCryptPasswordEncoder.encode(password);
+
+        ourUser.setPassword(encryptedPassword);
         userRepository.save(ourUser);
         recoveryTokenRepository.delete(token);
     }
