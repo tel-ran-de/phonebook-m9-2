4a7d6b4e5c0660641320484cc6089bf757f282b8
andre.reutow@gmail.com <andre.reutow@gmail.com>
2020-08-03 10:30:36 +0200

    add exceptions test

--- phonebook-api/src/test/java/com/telran/phonebookapi/service/UserServiceIntegrationTest.java
+++ phonebook-api/src/test/java/com/telran/phonebookapi/service/UserServiceIntegrationTest.java
@@ -1,5 +1,8 @@
 package com.telran.phonebookapi.service;
 
+import com.telran.phonebookapi.dto.UserDto;
+import com.telran.phonebookapi.exception.TokenNotFoundException;
+import com.telran.phonebookapi.exception.UserAlreadyExistsException;
 import com.telran.phonebookapi.model.ActivationToken;
 import com.telran.phonebookapi.model.User;
 import com.telran.phonebookapi.persistance.IActivationTokenRepository;
@@ -8,7 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -59,8 +61,19 @@ class UserServiceIntegrationTest {
         activationTokenRepository.save(activationToken);
 
         String tokenFalse = "111111111111111111";
-        Exception exception = assertThrows(EntityNotFoundException.class, () -> userService.activateUser(tokenFalse));
+        Exception exception = assertThrows(TokenNotFoundException.class, () -> userService.activateUser(tokenFalse));
         assertTrue(exception.getMessage().contains(UserService.NOT_ACTIVE_LINK));
         assertFalse(userRepository.findById(user.getEmail()).get().isActive());
+    }
+
+    @Test()
+    public void testActivateUser_userAlreadyExist() {
+        User user = new User("ivanov@gmail.com", "12345");
+        UserDto userDto = new UserDto(user.getEmail(), user.getPassword());
+
+        userRepository.save(user);
+
+        Exception exception = assertThrows(UserAlreadyExistsException.class, () -> userService.addUser(userDto));
+        assertTrue(exception.getMessage().contains(UserService.USER_ALREADY_EXISTS));
     }
 }
