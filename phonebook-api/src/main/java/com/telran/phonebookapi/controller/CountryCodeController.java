32ce51323d4ddb0c65c36256e97a6a5e48fc2fa6
Arina Mashkova <52415705+CharlieTono@users.noreply.github.com>
2020-09-14 21:23:58 +0200

    Pb 36 (#29)
    Country Code

---
+++ phonebook-api/src/main/java/com/telran/phonebookapi/controller/CountryCodeController.java
@@ -0,0 +1,48 @@
+package com.telran.phonebookapi.controller;
+
+import com.telran.phonebookapi.dto.CountryCodeDto;
+import com.telran.phonebookapi.mapper.CountryCodeMapper;
+import com.telran.phonebookapi.service.CountryCodeService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/country_code")
+public class CountryCodeController {
+
+    CountryCodeService countryCodeService;
+    CountryCodeMapper countryCodeMapper;
+
+    public CountryCodeController(CountryCodeService countryCodeService, CountryCodeMapper countryCodeMapper) {
+        this.countryCodeService = countryCodeService;
+        this.countryCodeMapper = countryCodeMapper;
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("")
+    public void addCode(@RequestBody @Valid CountryCodeDto codeDto) {
+        countryCodeService.add(codeDto.code, codeDto.country);
+    }
+
+    @GetMapping("/{id}")
+    public CountryCodeDto getById(@PathVariable int id) {
+        return countryCodeMapper.mapCountryCodeToDto(countryCodeService.getById(id));
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("/{id}")
+    public void removeById(@PathVariable int id) {
+        countryCodeService.removeById(id);
+    }
+
+    @GetMapping("")
+    public List<CountryCodeDto> getAllCountryCodes() {
+        return countryCodeService.getAllCountryCodes().stream()
+                .map(countryCodeMapper::mapCountryCodeToDto)
+                .collect(Collectors.toList());
+    }
+}
