765b8ad13724b63d234461fffa8091dcad4b5a8a
Arina Mashkova <52415705+CharlieTono@users.noreply.github.com>
2020-09-29 20:18:27 +0200

    Pb 39 (#44)

--- phonebook-api/src/main/java/com/telran/phonebookapi/model/TesterUser.java
+++ phonebook-api/src/main/java/com/telran/phonebookapi/model/TesterUser.java
@@ -5,6 +5,7 @@ import com.telran.phonebookapi.persistance.IUserRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,21 +14,26 @@ public class TesterUser implements CommandLineRunner {
 
     IUserRepository userRepository;
     IContactRepository contactRepository;
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Value("${com.telran.tester.user.email}")
     String email;
     @Value("${com.telran.tester.user.password}")
     String password;
 
-    public TesterUser(IUserRepository userRepository, IContactRepository contactRepository) {
+    public TesterUser(IUserRepository userRepository,
+                      IContactRepository contactRepository,
+                      BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.contactRepository = contactRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
     public void run(String... args) throws Exception {
         if (userRepository.findById(email).isEmpty()) {
-            User testUser = new User(email, password);
+            String encodedPassword = bCryptPasswordEncoder.encode(password);
+            User testUser = new User(email, encodedPassword);
             testUser.addRole(UserRole.USER);
             testUser.setActive(true);
             Contact profile = new Contact();
