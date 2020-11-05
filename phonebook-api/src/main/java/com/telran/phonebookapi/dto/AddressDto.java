eb7f80d4e48c1c34367f38464b30d1d5f2ad86fc
CharlieTono <arina.ershova90@gmail.com>
2020-11-05 19:17:41 +0100

    adds validation for dto

--- phonebook-api/src/main/java/com/telran/phonebookapi/dto/AddressDto.java
+++ phonebook-api/src/main/java/com/telran/phonebookapi/dto/AddressDto.java
@@ -3,12 +3,14 @@ package com.telran.phonebookapi.dto;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Size;
+
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddressDto {
 
     public int id;
-
+    @Size(max = 8, min = 3, message = "The zip-code is shorter than {min} or longer than {max}" )
     public String zip;
 
     public String country;
--- phonebook-api/src/main/java/com/telran/phonebookapi/dto/PhoneDto.java
+++ phonebook-api/src/main/java/com/telran/phonebookapi/dto/PhoneDto.java
@@ -3,6 +3,8 @@ package com.telran.phonebookapi.dto;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Size;
+
 @NoArgsConstructor
 @AllArgsConstructor
 public class PhoneDto {
@@ -11,6 +13,7 @@ public class PhoneDto {
 
     public int countryCode;
 
+    @Size(min = 9, max = 15)
     public long phoneNumber;
 
     public int contactId;
