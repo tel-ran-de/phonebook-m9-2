4b3e1efb894cc06518a4b8990a98d88038dcc0d7
Yustus7 <66375528+Yustus7@users.noreply.github.com>
2020-11-05 19:50:46 +0100

    add UserProfile (#62)

---
+++ phonebook-ui/src/app/user-page/user-profile/user-profile.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import {Contact} from "../../model/contact";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ContactsService} from "../../service/contact.service";
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent implements OnInit {
+
+
+  id: number;
+  contact: Contact;
+  email: string;
+
+  constructor(
+    public activatedRoute: ActivatedRoute,
+    public router: Router,
+    public contactsService: ContactsService
+  ) {
+    this.contact = new Contact();
+  }
+
+  ngOnInit() {
+    this.id = this.activatedRoute.snapshot.params.id;
+    this.contactsService.getContactById(this.id).subscribe(response => {
+      this.contact = response;
+    });
+  }
+
+
+  updatePhone(id: number) {
+    this.router.navigate(['/user/update-phone/'+ id]);
+  }
+  updateEmail(id: number) {
+    this.router.navigate(['/user/update-email/'+ id]);
+  }
+  updateAddress(id: number) {
+    this.router.navigate(['/user/update-address/'+ id]);
+  }
+  updateContact(id: number) {
+    this.router.navigate(['/user/update-contact/'+ id]);
+  }
+
+
+  removePhone(id: number) {
+    this.contactsService.removePhone(id).subscribe(() => {
+      this.contact.phoneNumbers = this.contact.phoneNumbers.filter(phone => phone.id != id)
+    })
+  }
+  removeEmail(id: number) {
+    this.contactsService.removeEmail(id).subscribe(() => {
+      this.contact.emails = this.contact.emails.filter(email => email.id != id)
+    })
+  }
+  removeAddress(id: number) {
+    this.contactsService.removeAddress(id).subscribe(() => {
+      this.contact.addresses = this.contact.addresses.filter(address => address.id != id)
+    })
+  }
+
+}
